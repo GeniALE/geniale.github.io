827c323b3466ec3078e5f192a741ddedc62808a1
AimeMelancon <aimemelancon@gmail.com>
2025-06-27 19:14:45 -0400

    commencement de la page des produits

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,11 +1,11 @@
-<!-- eslint-disable prettier/prettier -->
-<script setup lang="ts"></script>
-<!-- eslint-disable prettier/prettier -->
-<template>
-  <section>
-    <div class="text-center text-3xl font-bold pt-10 pb-5">
-      
-      <p class="underline">Cette page est en construction. / This page is under construction.</p>
-    </div>
-  </section>
+<script setup lang="ts">
+import ProductItemComponent from '@geniale/components/ProductItemComponent.vue';
+</script>
+
+<template v-for="product in products">
+  <ProductItemComponent
+    :name="products.name"
+    :img="products.img"
+    :description="products.description"
+  />
 </template>
