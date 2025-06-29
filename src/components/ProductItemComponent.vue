b4a81be9e600728d50a1d0db8a187b243372aa48
AimeMelancon <aimemelancon@gmail.com>
2025-06-28 22:42:10 -0400

    productView fonctionne pour l'affichage des product items, mais il faut aligner les items pour que l'image soit sur le côté droit et le reste sur le côté gauche

--- src/components/ProductItemComponent.vue
+++ src/components/ProductItemComponent.vue
@@ -6,18 +6,20 @@ defineProps<{
 }>();
 </script>
 <template>
-  <div class="mt-4 pt-2">
-    <div class="flex flex-col items-center text-center rounded-full p-3 py-4">
+  <div class="mt-4 pt-2 group hover:shadow-lg transition-shadow duration-300">
+    <div class="flex flex-nowrap justify-center">
+      <div class="mt-5">
+        <h4 class="text-lg font-medium mb-0 text-center">
+          {{ name }}
+        </h4>
+        <br />
+        <small class="text-muted margin-5">{{ description }}</small>
+      </div>
       <img
         :src="img"
-        class="rounded-full h-64 w-64 object-cover shadow-md"
-        alt="GaniAle member"
+        class="h-64 w-64 object-cover shadow-md justify-normal rounded-lg"
+        alt="Product Image"
       />
-      <div class="mt-3">
-        <h4 class="text-lg font-medium mb-0">{{ name }}</h4>
-        <br />
-        <small class="text-muted">{{ description }}</small>
-      </div>
     </div>
   </div>
 </template>
